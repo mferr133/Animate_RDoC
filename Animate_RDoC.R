7baa90128efff6ca04acd21b09792630b56d961a
mferr133 <42357812+mferr133@users.noreply.github.com>
2020-08-21 17:36:50 -0400

    Update Animate_RDoC.R

--- Animate_RDoC.R
+++ Animate_RDoC.R
@@ -6,15 +6,15 @@ sapply(load.lib,require,character=TRUE)
 df <- read.csv(file = 'data.csv') # Loads the dataset
 df$Biotype <- as.character(df$Biotype)
 Animate_RDoC <- ggplot(df, aes(SP, CC, size = PROMHBQ , color = Biotype)) + # Set up ggplot & animate specific bits:
-    geom_text(aes(label = patient), nudge_y = +0.015, show.legend = FALSE) +
-    stat_smooth(formula = y ~ log10(x), se = FALSE, size = 0.7, linetype="solid") +  #these are the 3 colored fitting lines
-    labs(title = '{round((frame_time),digits=0)} Days Post-Intervention', x = 'Low-Function    <-     Social Processes Score     ->    High-Function', y = 'Low-Function    <-    Cognitive Control Score     ->    High-Function') +
-    guides(color = guide_legend(order=1), size = guide_legend(order=2)) + 
-    view_follow(fixed_y = TRUE) + 
-    theme_bw() + 
-    transition_time(day) + 
-    ease_aes('linear') + 
-    geom_point(shape=1) + 
-    theme(legend.position=c(0.9,0.15))  
-animate(Animate_RDoC, height = 800, width = 800)
+  geom_text(aes(label = patient), nudge_y = +0.016, show.legend = FALSE) +
+  stat_smooth(formula = y ~ log10(x), se = FALSE, size = 0.8, linetype="solid") +  #these are the 3 colored fitting lines
+  labs(title = '{round((frame_time),digits=0)} Days Post-Intervention', x = 'Low-Function    <-     Social Processes Score     ->    High-Function', y = 'Low-Function    <-    Cognitive Control Score     ->    High-Function') +
+  guides(color = guide_legend(order=1), size = guide_legend(order=2)) + 
+  view_follow(fixed_y = TRUE) + 
+  theme_bw() + 
+  transition_time(day) + 
+  ease_aes('linear') + 
+  geom_point(shape=16) + 
+  theme(legend.position=c(0.9,0.15))  
+animate(Animate_RDoC, height = 1000, width = 1000)
 anim_save("Animate_RDoC.gif")
